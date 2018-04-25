dd200a8b09d1c6d050d9babd756954698efd987f
Jason M. Goncalves <jgoncalves@protonmail.com>
2018-04-24 20:44:13 -0400

    created burger database, and inserted data into table

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+-- Seed Burgers into Database
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Big Mac", false),
+  ("Whooper", false),
+  ("Cheezburger", false),
+  ("Baconator", false),
+  ("Quarter Pounder", false),
+  ("McRib", false),
+  ("Double Double", true);
